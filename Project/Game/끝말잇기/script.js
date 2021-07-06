67c83c71ece1c2aa0e1701975ab0cd4314bd84e9
Pulan <ekgus5095@gmail.com>
2021-07-06 19:04:40 +0900

    Create JS_Game

--- Project/Game/끝말잇기/script.js
+++ Project/Game/끝말잇기/script.js
@@ -1,40 +1,29 @@
-const number = parseInt(prompt("몇 명이 참가하나요?"), 10);
+const number = Number(prompt("몇 명이 참가하나요?"));
 const $button = document.querySelector("button");
 const $input = document.querySelector("input");
 const $word = document.querySelector("#word");
-let word;
-let newWord;
+const $order = document.querySelector("#order");
+let word; // 제시어
+let newWord; // 새로 입력한 단어
 
 const onClickButton = () => {
-  if (!word) {
+  if (!word || word[word.length - 1] === newWord[0]) {
     // 제시어가 비어 있는가?
     // 비어 있다.
     word = newWord; // 입력한 단어가 제시어가 된다.
     $word.textContent = word;
-    $input.value = "";
-    const order = Number($order.textContent);
+    const order = Number($order.textContent); // 현재 순서
     if (order + 1 > number) {
       $order.textContent = 1;
     } else {
       $order.textContent = order + 1;
     }
   } else {
-    // 비어 있지 않다.
-    if (word[word.length - 1] === newWord[0]) {
-      // 올바른가
-      word = newWord; // 입력한 단어가 제시어가 된다.
-      $word.textContent = word;
-      $input.value = "";
-      const order = Number($order.textContent); // 현재 순서
-      if (order + 1 > number) {
-        $order.textContent = 1;
-      } else {
-        $order.textContent = order + 1;
-      }
-    } else {
-      // 올바르지 않는가
-    }
+    // 올바르지 않은가
+    alert("올바르지 않은 단어입니다!");
   }
+  $input.value = "";
+  $input.focus();
 };
 const onInput = (event) => {
   newWord = event.target.value;
