8732d33bac36d857ea805172dbfef6d4d1d7cbc0
Pulan <ekgus5095@gmail.com>
2021-07-18 15:56:45 +0900

    Building a basic boilerplate using MongoDB and React

--- Project/Practical Example/Jhon Ahn/boiler-plate/server/index.js
+++ Project/Practical Example/Jhon Ahn/boiler-plate/server/index.js
@@ -23,6 +23,10 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World!~~ "));
 
+app.get("/api/hello", (req, res) => {
+  res.send("Hello World~");
+});
+
 app.post("/api/users/register", (req, res) => {
   //회원 가입 할떄 필요한 정보들을  client에서 가져오면
   //그것들을  데이터 베이스에 넣어준다.
