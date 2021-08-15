7a2650104758e4625bc3ec04eae4b679483baa29
Pulan <ekgus5095@gmail.com>
2021-08-15 19:43:05 +0900

    Added login function of To-Do app

---
+++ Project/Practical Example/WebSite/ToDo/app.js
@@ -0,0 +1,30 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+const link = document.querySelector("a");
+
+const onLoginSubmit = (e) => {
+  e.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+};
+
+const paintGreetings = (username) => {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+};
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
